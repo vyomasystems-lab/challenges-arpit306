bba1f3e3e9b6da0b644533c36ac89563946b4c1e
Arpit Sharma <arpitniraliya306@gmail.com>
2022-07-29 14:32:04 +0000

    testing for 12th input

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,5 +6,17 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    #test for 12th input
+    input12=0b00
+    input13=0b10
+    select=0b01100 #12th line
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    #input driving
+    dut.inp12.value=input12
+    dut.inp13.value=input13
+    dut.sel.value=select
+
+    await Timer(2, units='ns')
+
+    cocotb.log.info(f'input12={input12:0b00} select={select:0b01100} DUT:{dut.out.value:0b00}')
+    assert dut.out.value==0b00,f"Mux output is incorrect:{dut.X.value}!=0b00"

aa82974877c4ae4d64d44d5a11bdca7ab42d2dcb
Arpit Sharma <arpitniraliya306@gmail.com>
2022-07-30 10:21:00 +0000

    bug 1, 13th input sel line error

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -20,3 +20,19 @@ async def test_mux(dut):
 
     #cocotb.log.info(f'input12={input12:0b00} select={select:0b01101} model={input13:0b11} DUT:{dut.out.value:0b00}')
     assert dut.out.value == 0b11, f"Mux output is incorrect: {dut.out.value} != 0b11"
+
+@cocotb.test()
+async def test2_mux(dut):
+    """Test for mux2"""
+    #test for 30th input
+    input30=0b00
+    select=0b11111 #30th line
+
+    #input driving
+    dut.inp30.value=input30
+    dut.sel.value=select
+
+    await Timer(2, units='ns')
+
+    #cocotb.log.info(f'input12={input12:0b00} select={select:0b01101} model={input13:0b11} DUT:{dut.out.value:0b00}')
+    assert dut.out.value == 0b00, f"Mux output is incorrect: {dut.out.value} != 0b00"

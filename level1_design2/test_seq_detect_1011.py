078c9baaf7b35a5f00503b21e4f23ac5dbaa7529
Arpit Sharma <arpitniraliya306@gmail.com>
2022-07-30 12:10:50 +0000

    all bugs cases

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -72,3 +72,78 @@ async def test_seq_bug2(dut):
     input=0b1
     dut.inp_bit.value=input          #input driving
     assert dut.seq_seen.value == 0b1, f"output is incorrect: {dut.seq_seen.value} != 0b1"
+
+@cocotb.test()
+async def test_seq_bug2(dut):
+    """Test for seq detection """
+    
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+                                    #Test for input pattern 1011011
+    input=0b1
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b0, f"output is incorrect: {dut.seq_seen.value} != 0b0"
+    input=0b0
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b0, f"output is incorrect: {dut.seq_seen.value} != 0b0"
+    input=0b1
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b0, f"output is incorrect: {dut.seq_seen.value} != 0b0"
+    input=0b1
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b1, f"output is incorrect: {dut.seq_seen.value} != 0b1"
+    input=0b0
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b0, f"output is incorrect: {dut.seq_seen.value} != 0b0"
+    input=0b1
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b0, f"output is incorrect: {dut.seq_seen.value} != 0b0"
+    input=0b1
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b1, f"output is incorrect: {dut.seq_seen.value} != 0b1"
+
+@cocotb.test()
+async def test_seq_bug2(dut):
+    """Test for seq detection """
+    
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+                                    #Test for input pattern 10111011
+    input=0b1
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b0, f"output is incorrect: {dut.seq_seen.value} != 0b0"
+    input=0b0
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b0, f"output is incorrect: {dut.seq_seen.value} != 0b0"
+    input=0b1
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b0, f"output is incorrect: {dut.seq_seen.value} != 0b0"
+    input=0b1
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b1, f"output is incorrect: {dut.seq_seen.value} != 0b1"
+    input=0b1
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b0, f"output is incorrect: {dut.seq_seen.value} != 0b0"
+    input=0b0
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b0, f"output is incorrect: {dut.seq_seen.value} != 0b0"
+    input=0b1
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b0, f"output is incorrect: {dut.seq_seen.value} != 0b0"
+    input=0b1
+    dut.inp_bit.value=input          #input driving
+    assert dut.seq_seen.value == 0b1, f"output is incorrect: {dut.seq_seen.value} != 0b1"

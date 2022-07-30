9296dcaefc6ad25392618a20c7d055ba691f129b
Arpit Sharma <arpitniraliya306@gmail.com>
2022-07-30 11:38:48 +0000

    test

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -13,7 +13,7 @@ from cocotb.triggers import RisingEdge, FallingEdge
 @cocotb.test()
 async def test_seq_bug1(dut):
     """Test for seq detection """
-
+    
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
 
@@ -23,29 +23,19 @@ async def test_seq_bug1(dut):
     dut.reset.value = 0
     await FallingEdge(dut.clk)
 
+                                    #Test for input pattern 11011
     input=0b1
     dut.inp_bit.value=input          #input driving
-    #await Timer(12, units='us')
     assert dut.seq_seen.value == 0b0, f"seq_seenput is incorrect: {dut.seq_seen.value} != 0b0"
-    
     input=0b1
     dut.inp_bit.value=input          #input driving
-    #await Timer(12, units='us')
-    assert dut.seq_seen.value == 0b0, f"seq_seenput is incorrect: {dut.seq_seen.value} != 0b0"
-       
+    assert dut.seq_seen.value == 0b0, f"seq_seenput is incorrect: {dut.seq_seen.value} != 0b0"  
     input=0b0
     dut.inp_bit.value=input          #input driving
-    #await Timer(12, units='us')
     assert dut.seq_seen.value == 0b0, f"seq_seenput is incorrect: {dut.seq_seen.value} != 0b0"
-
     input=0b1
     dut.inp_bit.value=input          #input driving
-    #await Timer(12, units='us')
     assert dut.seq_seen.value == 0b0, f"seq_seenput is incorrect: {dut.seq_seen.value} != 0b0"
-
     input=0b1
     dut.inp_bit.value=input          #input driving
-    #await Timer(12, units='us')
     assert dut.seq_seen.value == 0b1, f"seq_seenput is incorrect: {dut.seq_seen.value} != 0b1"
-
-    #cocotb.log.info('#### CTB: Develop your test here! ######')
